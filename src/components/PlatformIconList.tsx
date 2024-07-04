6c411f44d01bba223228408b5943243b01289e60
Krishna kumar <krishnakumar02494@mail.com>
2024-07-05 00:02:42 +0530

    Added platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,41 @@
+import {
+  FaXbox,
+  FaPlaystation,
+  FaWindows,
+  FaApple,
+  FaAndroid,
+  FaLinux,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon } from "@chakra-ui/react";
+
+interface Props {
+  platform: Platform[];
+}
+
+function PlatformIconList({ platform }: Props) {
+  const iconMap: { [key: string]: React.ElementType } = {
+    xbox: FaXbox,
+    playstation: FaPlaystation,
+    windows: FaWindows,
+    apple: FaApple,
+    android: FaAndroid,
+    linux: FaLinux,
+    iphone: MdPhoneIphone,
+    nintendo: SiNintendo,
+    Web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={"10px"}>
+      {platform.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="grey.500" />
+      ))}
+    </HStack>
+  );
+}
+
+export default PlatformIconList;
